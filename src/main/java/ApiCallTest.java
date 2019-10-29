bb2f58e342fe9e437e4498a15475ee4efdca9b35
Kartik Dave <davekartik24@gmail.com>
2019-10-28 18:50:02 -0700

    Skier Application Client Structure Setup using Swagger
    In this commit:
    1. Client setup to interact with API using Swagger
    2. Example to send a post request

---
+++ src/main/java/ApiCallTest.java
@@ -0,0 +1,24 @@
+import io.swagger.client.ApiClient;
+import io.swagger.client.ApiException;
+import io.swagger.client.api.SkiersApi;
+import io.swagger.client.model.LiftRide;
+
+public class ApiCallTest {
+
+    public static void main(String[] args) {
+
+        String basePath = "http://ec2-54-186-128-171.us-west-2.compute.amazonaws.com:8080/SkierAssignment_war";
+        SkiersApi apiInstance = new SkiersApi();
+        ApiClient client = apiInstance.getApiClient();
+        client.setBasePath(basePath);
+        try {
+            LiftRide inputLiftRide = new LiftRide();
+            inputLiftRide.setLiftID(21);
+            inputLiftRide.setTime(271);
+            apiInstance.writeNewLiftRide(inputLiftRide,1, "2019", "1", 123);
+        } catch (ApiException e) {
+            System.err.println("Exception when calling ResortsApi#getResorts");
+            e.printStackTrace();
+        }
+    }
+}
